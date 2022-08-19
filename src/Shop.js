e260cabe20013fd86d0612f4cc768923abc6ff66
plokm1234 <wyuen@wais-mbp.lan>
2022-08-20 00:48:22 +0800

    fixed shopping cart

--- src/Shop.js
+++ src/Shop.js
@@ -2,117 +2,55 @@ import Navbar from "./components/Navbar.js";
 import pc from "./images/pcgamecase.jpg";
 import ps5 from "./images/ps5-game-case.jpg";
 import React, { useState } from "react";
-import CheckOut from "./components/checkOut"
 
 const ProductFactory = (title, price, quantity) => {
-  return { title, price, quantity };
+  return {
+    id: Math.random().toString(),
+    img: pc,
+    name: title,
+    price: price,
+    quantity: quantity,
+  }
 };
 
+const data = localStorage.hasOwnProperty("projectStorage") ? JSON.parse(localStorage.getItem("projectStorage")) : []
+
 const Shop = () => {
-  const [cartItems, setCartItems] = useState([]);
-  const [openStatus, setOpenStatus] = useState({shop: true, shoppingCart: false});
-
-  const store = () => {
-    localStorage.setItem("projectStorage", JSON.stringify(cartItems));
-  };
-
-  console.log(cartItems);
-
-  const shopClick = () => {
-    setOpenStatus({shop: true, shoppingCart: false})
-  }
-
-  const shoppingCartClick = () => {
-    setOpenStatus({shop: false, shoppingCart: true})
-  }
+  const [cartItems, setCartItems] = useState(data);
 
   const addToCart = (e) => {
     let cartItemCopy = [...cartItems];
     let item = JSON.parse(e.target.name);
     cartItemCopy.push(item);
-    store()
+    localStorage.setItem("projectStorage", JSON.stringify(cartItemCopy))
     setCartItems(cartItemCopy);
-  };
-
-  const removeClick = (e) => {
-    const newCartItems = cartItems.filter(
-      (cartItem) => cartItem.id !== e.target.id
-    );
-
-    setCartItems(newCartItems);
-  };
-
-  const decreaseClick = (e) => {
-    let newCartItems = {};
-
-    let foundIndex = cartItems.findIndex((item) => item.id === e.target.id);
-
-    if (cartItems[foundIndex].quantity === 1) {
-      newCartItems = cartItems.filter(
-        (cartItem) => cartItem.id !== e.target.id
-      );
-    } else {
-      newCartItems = cartItems.map((item) => {
-        if (item.id === e.target.id) {
-          return { ...item, quantity: item.quantity - 1 };
-        }
-        return item;
-      });
-    }
-    setCartItems(newCartItems);
-  };
-
-  const increaseClick = (e) => {
-    const newCartItems = cartItems.map((item) => {
-      if (item.id === e.target.id) {
-        return { ...item, quantity: item.quantity + 1 };
-      } else {
-      }
-      return item;
-    });
-
-    setCartItems(newCartItems);
+    console.log(cartItems);
   };
 
   const pcCopy = ProductFactory("PC edition", 399, 1);
   const ps5Copy = ProductFactory("PS5 edition", 429, 1);
 
-  if(openStatus.shop) {
-    return (
-      <div className="shop">
-        <div className="main">
-          <Navbar shop={shopClick} shoppingCart={shoppingCartClick}/>
-        </div>
-        { openStatus.shop &&
-          <div className="gamecopy">
-          <div>
-            <img src={pc} alt="pc game case" />
-            <button name={JSON.stringify(pcCopy)} onClick={addToCart}>
-              加入購物車
-            </button>
-          </div>
-          <div>
-            <img src={ps5} alt="ps5 game case" />
-            <button name={JSON.stringify(ps5Copy)} onClick={addToCart}>
-              加入購物車
-            </button>
-          </div>
-        </div>
-        }
+  return (
+    <div className="shop">
+      <div className="main">
+        <Navbar />
       </div>
-    )
-  }else{
-    return (
-      <div>
-        <div className="main">
-          <Navbar />
-        </div>  
-
-        <CheckOut somedata={"succeed"} cartItems={cartItems} removeClick={removeClick} decreaseClick={decreaseClick} increaseClick={increaseClick}/>
+      <div className="gamecopy">
+        <div>
+          <img src={pc} alt="pc game case" />
+          <button name={JSON.stringify(pcCopy)} onClick={addToCart}>
+            加入購物車
+          </button>
+        </div>
+        <div>
+          <img src={ps5} alt="ps5 game case" />
+          <button name={JSON.stringify(ps5Copy)} onClick={addToCart}>
+            加入購物車
+          </button>
+        </div>
       </div>
-        
-    )
-  }
+    </div>
+  );
 };
 
 export default Shop;
--- src/components/checkOut.js
+++ src/components/checkOut.js
@@ -2,84 +2,88 @@ import React, { useState } from "react";
 import Navbar from "./Navbar";
 import testItem from "../images/fb.png";
 import "../styles/checkOut.css";
-let test = {
-  id: 1,
-  img: testItem,
-  name: "FB",
-  price: 1,
-  quantity: 1,
-  remove: "X",
-};
+// let test = {
+//   id: 1,
+//   img: testItem,
+//   name: "FB",
+//   price: 1,
+//   quantity: 1,
+// };
 
-const CheckOut = ({somedata, cartItems, removeClick, decreaseClick, increaseClick}) => {
-  // const [cartItems, setCartItems] = useState([]);
-  // const cartItems = props.cartItems
+
+const CheckOut = () => {
+  const projectStoreUnParsed = localStorage.getItem("projectStorage");
+  const projectStoreParsed = JSON.parse(projectStoreUnParsed); 
+  const [cartItemsCopy, setCartItemsCopy] = useState(projectStoreParsed);
+
+  console.log(cartItemsCopy)
 
   // const testClick = () => {
   //   test = {
   //     ...test,
   //     id: Math.random().toString(),
   //   };
-
-  //   setCartItems([...cartItems, test]);
+  //   console.log(test)
+  //   setCartItemsCopy([...cartItemsCopy, test]);
   // };
 
-  // const removeClick = (e) => {
-  //   const newCartItems = cartItems.filter(
-  //     (cartItem) => cartItem.id !== e.target.id
-  //   );
+  const removeClick = (e) => {
+    const newCartItems = cartItemsCopy.filter(
+      (cartItem) => cartItem.id !== e.target.id
+    );
+    
+    localStorage.setItem("projectStorage", JSON.stringify(newCartItems))
+    setCartItemsCopy(newCartItems);
+  };
 
-  //   setCartItems(newCartItems);
-  // };
+  const decreaseClick = (e) => {
+    let newCartItems = {};
 
-  // const decreaseClick = (e) => {
-  //   let newCartItems = {};
+    let foundIndex = cartItemsCopy.findIndex((item) => item.id === e.target.id);
 
-  //   let foundIndex = cartItems.findIndex((item) => item.id === e.target.id);
+    console.log(foundIndex)
+    if (cartItemsCopy[foundIndex].quantity === 1) {
+      newCartItems = cartItemsCopy.filter(
+        (cartItem) => cartItem.id !== e.target.id
+      );
+    } else {
+      newCartItems = cartItemsCopy.map((item) => {
+        if (item.id === e.target.id) {
+          return { ...item, quantity: item.quantity - 1 };
+        }
+        return item;
+      });
+    }
+ 
+    localStorage.setItem("projectStorage", JSON.stringify(newCartItems))
+    setCartItemsCopy(newCartItems);
+  };
 
-  //   if (cartItems[foundIndex].quantity === 1) {
-  //     newCartItems = cartItems.filter(
-  //       (cartItem) => cartItem.id !== e.target.id
-  //     );
-  //   } else {
-  //     newCartItems = cartItems.map((item) => {
-  //       if (item.id === e.target.id) {
-  //         return { ...item, quantity: item.quantity - 1 };
-  //       }
-  //       return item;
-  //     });
-  //   }
-  //   setCartItems(newCartItems);
-  // };
+  const increaseClick = (e) => {
+    const newCartItems = cartItemsCopy.map((item) => {
+      if (item.id === e.target.id) {
+        return { ...item, quantity: item.quantity + 1 };
+      } else {
+      }
+      return item;
+    });
+    console.log(newCartItems)
 
-  // const increaseClick = (e) => {
-  //   const newCartItems = cartItems.map((item) => {
-  //     if (item.id === e.target.id) {
-  //       return { ...item, quantity: item.quantity + 1 };
-  //     } else {
-  //     }
-  //     return item;
-  //   });
-
-  //   setCartItems(newCartItems);
-  // };
-  const projectStoreUnParsed = localStorage.getItem("projectStorage");
-  const projectStoreParsed = JSON.parse(projectStoreUnParsed);
+    localStorage.setItem("projectStorage", JSON.stringify(newCartItems))
+    setCartItemsCopy(newCartItems);
+  };
 
   return (
     <div className="checkOutPage">
       <div className="main">
         <Navbar />
       </div>
-      <div>
-        {console.log(projectStoreParsed)}
-        <h1>{somedata}</h1>
-      </div>
+
       {/* <div style={{ margin: "100px" }}>
         <button onClick={testClick}>測試</button>
       </div> */}
 
-      {/* <div className="checkout-container">
+      <div className="checkout-container">
         <div className="checkout-header">
           <span>產品</span>
           <span>描述</span>
@@ -87,7 +91,7 @@ const CheckOut = ({somedata, cartItems, removeClick, decreaseClick, increaseClic
           <span>價格</span>
           <span>移除</span>
         </div>
-        {cartItems.map((cartItem) => {
+        {cartItemsCopy.map((cartItem) => {
           const { id, img, name, price, quantity } = cartItem;
 
           return (
@@ -105,12 +109,12 @@ const CheckOut = ({somedata, cartItems, removeClick, decreaseClick, increaseClic
               </span>
               <span>{price}</span>
               <button id={id} onClick={removeClick}>
-                {remove}
+                x
               </button>
             </div>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 };
