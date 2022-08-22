f58d06232d0a96b40809554656822c467bb4222b
domodfg <domodfg@yahoo.com.hk>
2022-08-22 15:42:46 +0800

    Fixed the bugs in shopping cart

--- src/components/checkOut.js
+++ src/components/checkOut.js
@@ -1,64 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "./Navbar";
 import "../styles/checkOut.css";
 
 const CheckOut = () => {
-  const data = localStorage.hasOwnProperty("projectStorage") ? JSON.parse(localStorage.getItem("projectStorage")) : []
   const [total, setTotal] = useState(0);
-  const [cartItemsCopy, setCartItemsCopy] = useState(data);
+  const [cartItems, setCartItems] = useState([]);
+  const firstRun = useRef(true);
 
-  const add = (accumulator, {quantity, price}) => {
+  const add = (accumulator, { quantity, price }) => {
     return accumulator + quantity * price;
-  }
+  };
 
-  useEffect(() => { 
-    setTotal(cartItemsCopy.reduce(add, 0));
-  
-   }, [cartItemsCopy]);
+  useEffect(() => {
+    const cartItems = JSON.parse(localStorage.getItem("projectStorage"));
+    if (cartItems) {
+      setCartItems(cartItems);
+    }
+  }, []);
 
-  
+  useEffect(() => {
+    setTotal(cartItems.reduce(add, 0));
+    if (!firstRun.current) {
+      localStorage.setItem("projectStorage", JSON.stringify(cartItems));
+    }
+    firstRun.current = false;
+  }, [cartItems]);
+
   const removeClick = (e) => {
-    const newCartItems = cartItemsCopy.filter(
+    const newCartItems = cartItems.filter(
       (cartItem) => cartItem.id !== e.target.id
     );
-    
-    localStorage.setItem("projectStorage", JSON.stringify(newCartItems))
-    setCartItemsCopy(newCartItems);
+    setCartItems(newCartItems);
   };
 
   const decreaseClick = (e) => {
     let newCartItems = {};
 
-    let foundIndex = cartItemsCopy.findIndex((item) => item.id === e.target.id);
+    let foundIndex = cartItems.findIndex((item) => item.id === e.target.id);
 
-    if (cartItemsCopy[foundIndex].quantity === 1) {
-      newCartItems = cartItemsCopy.filter(
+    if (cartItems[foundIndex].quantity === 1) {
+      newCartItems = cartItems.filter(
         (cartItem) => cartItem.id !== e.target.id
       );
     } else {
-      newCartItems = cartItemsCopy.map((item) => {
+      newCartItems = cartItems.map((item) => {
         if (item.id === e.target.id) {
           return { ...item, quantity: item.quantity - 1 };
         }
         return item;
       });
     }
- 
-    localStorage.setItem("projectStorage", JSON.stringify(newCartItems))
-    setCartItemsCopy(newCartItems);
+    setCartItems(newCartItems);
   };
 
   const increaseClick = (e) => {
-    const newCartItems = cartItemsCopy.map((item) => {
+    const newCartItems = cartItems.map((item) => {
       if (item.id === e.target.id) {
         return { ...item, quantity: item.quantity + 1 };
       } else {
       }
       return item;
     });
-
-    localStorage.setItem("projectStorage", JSON.stringify(newCartItems))
-    setCartItemsCopy(newCartItems);
+    setCartItems(newCartItems);
   };
 
   return (
@@ -68,47 +71,63 @@ const CheckOut = () => {
       </div>
 
       <div className="checkout-container">
-        
         <div className="left-container">
           <ul>
-            <li> 
-              {cartItemsCopy.map((cartItem) => {
-                  const { id, img, name, price, quantity } = cartItem;
+            <li>
+              {cartItems.map((cartItem) => {
+                const { id, img, name, price, quantity } = cartItem;
 
-                  return (
-                      <div className="product-container">
-                        <div className="img-details">{<img src={img} width="160px" height="200px" alt="product" />}</div>
-                        <div className="details-container">
-                          <div className="name-details">{name}</div>
-                          <div className="button-container">
-                            <button id={id} onClick={decreaseClick} className="btn btn-light">
-                              &#8595;
-                            </button>
-                            {quantity}
-                            
-                            <button id={id} onClick={increaseClick} className="btn btn-light">
-                              &#8593;
-                            </button>
-                          </div>
-                          <div className="price-details">HKD ${price}</div>
-                          <div className="remove-details">
-                            <button id={id} onClick={removeClick} className="btn btn-danger">
-                              移除
-                            </button>
-                          </div>
-                        </div>
+                return (
+                  <div key={id} className="product-container">
+                    <div className="img-details">
+                      {
+                        <img
+                          src={img}
+                          width="160px"
+                          height="200px"
+                          alt="product"
+                        />
+                      }
+                    </div>
+                    <div className="details-container">
+                      <div className="name-details">{name}</div>
+                      <div className="button-container">
+                        <button
+                          id={id}
+                          onClick={decreaseClick}
+                          className="btn btn-light"
+                        >
+                          &#8595;
+                        </button>
+                        {quantity}
+
+                        <button
+                          id={id}
+                          onClick={increaseClick}
+                          className="btn btn-light"
+                        >
+                          &#8593;
+                        </button>
                       </div>
-                  )
-                })}
+                      <div className="price-details">HKD ${price}</div>
+                      <div className="remove-details">
+                        <button
+                          id={id}
+                          onClick={removeClick}
+                          className="btn btn-danger"
+                        >
+                          移除
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </li>
           </ul>
         </div>
 
-        {
-          <div className="total right-container">
-            帳單總額: ${total}
-          </div>
-        }
+        {<div className="total right-container">帳單總額: ${total}</div>}
       </div>
     </div>
   );
