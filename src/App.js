95d252341d2864c3d3b7c5a5dccc660da23b03ee
domodfg <domodfg@yahoo.com.hk>
2022-08-23 12:05:27 +0800

    Add background video on the main page

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ import Gameinfo from "./components/Gameinfo.js";
 import News from "./components/News.js";
 import Footer from "./components/footer.js";
 import Map from "./components/Map.js";
+import mainvideo from "./images/main.webm";
 
 function App() {
   return (
     <div className="App">
       <div className="main">
         <Navbar />
+        <video autoPlay muted loop>
+          <source src={mainvideo} type="video/mp4" />
+        </video>
         <div>
           <h1 className="logo">World's Bole</h1>
           <p>動作幻想 RPG</p>
