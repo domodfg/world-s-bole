0b7bcfb0e9f44ccee0007bf21f4ec26a57945f53
plokm1234 <58145843+plokm1234@users.noreply.github.com>
2022-08-23 14:54:11 +0800

    added responsive layout to the shopping chart page

--- src/components/checkOut.js
+++ src/components/checkOut.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigate  } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import "../styles/checkOut.css";
 
 const CheckOut = () => {
   const [total, setTotal] = useState(0);
+  const [shippingCost, setShippingCost] = useState(0);
   const [cartItems, setCartItems] = useState([]);
   const [isEmpty, setIsEmpty] = useState(true);
   const firstRun = useRef(true);
@@ -26,18 +27,18 @@ const CheckOut = () => {
       localStorage.setItem("projectStorage", JSON.stringify(cartItems));
     }
     firstRun.current = false;
-    
-    if(cartItems.length === 0){
+
+    if (cartItems.length === 0) {
       setIsEmpty(true)
-    }else {
+    } else {
       setIsEmpty(false)
     }
   }, [cartItems]);
 
-  
-  let navigate = useNavigate(); 
+
+  let navigate = useNavigate();
   const changeRoute = () => {
-    let path = `/shop`; 
+    let path = `/shop`;
     navigate(path);
   }
 
@@ -78,79 +79,101 @@ const CheckOut = () => {
     setCartItems(newCartItems);
   };
 
-  if(isEmpty) {
+  if (isEmpty) {
     return (
       <div className="checkOutPage">
         <div className="main">
           <Navbar />
         </div>
         <div className="empty-shopping-cart">
-          <div>
+          <div className="empty-shopping-cart-desc">
             購物車沒有貨品
           </div>
           <div>
             <button onClick={changeRoute} className="shop-now-btn">
               立即購物
-              </button>
+            </button>
           </div>
         </div>
       </div>
     )
-  }else{
+  } else {
     return (
       <div className="checkOutPage">
         <div className="main">
           <Navbar />
         </div>
-  
+
         <div className="checkout-container">
           <div className="left-container">
-                {cartItems.map((cartItem) => {
-                    const { id, img, name, price, quantity } = cartItem;
-  
-                    return (
-                      <div key={id} className="list-item"> 
-                        <div className="product-container">
-                          <div className="img-details">
-                            {<img src={img} width="160px" height="200px" alt="product" />}
-                          </div>
-                          <div className="details-container">
-                            <div className="name-details">
-                              {name}
-                            </div>
-                            <div className="button-container">
-                              <button id={id} onClick={decreaseClick} className="increase-btn">
-                                &#8595;
-                              </button>
-                              {quantity}
-                              <button id={id} onClick={increaseClick} className="decrease-btn">
-                                &#8593;
-                              </button>
-                            </div>
-                            <div className="price-details">HKD ${price}</div>
-                            <div className="remove-details">
-                              <button id={id} onClick={removeClick} className="remove-btn">
-                                移除
-                              </button>
-                            </div>
-                          </div>
-                        </div>
+            {cartItems.map((cartItem) => {
+              const { id, img, name, price, quantity } = cartItem;
+
+              return (
+                <div key={id} className="list-item">
+                  <div className="product-container">
+                    <div className="img-container">
+                      {
+                        <img className="img-details" src={img} width="160px" height="200px" alt="product" />
+                      }
+                    </div>
+                    <div className="details-container">
+                      <div className="name-details">
+                        {name}
                       </div>
-                    )
-                  })}
+                      <div className="button-container">
+                        <button id={id} onClick={decreaseClick} className="increase-btn">
+                          &#8595;
+                        </button>
+                        {quantity}
+                        <button id={id} onClick={increaseClick} className="decrease-btn">
+                          &#8593;
+                        </button>
+                      </div>
+                      <div className="price-details">
+                        HKD ${price}
+                      </div>
+                      <div className="remove-details">
+                        <button id={id} onClick={removeClick} className="remove-btn">
+                          移除
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
           </div>
-  
+
           {
             <div className="right-container">
               <div>
-                <div className="space-between-container"><div>小計:</div> <div> ${total} </div></div>
-                <div className="space-between-container"><div>運費:</div> <div>$0</div></div>
+                <div className="space-between-container">
+                  <div>
+                    小計:
+                  </div> 
+                  <div> 
+                    ${total} 
+                  </div>
+                </div>
+                <div className="space-between-container">
+                  <div>
+                    運費:
+                  </div> 
+                  <div>
+                    ${shippingCost}
+                  </div>
+                </div>
               </div>
               <div className="space-between-container">
-                <div>總額</div>
-                <div>${total + 0}</div>
+                <div>
+                  總額:
+                </div>
+                <div>
+                  ${total + shippingCost}
+                </div>
               </div>
-              <div>
+              <div className="payment-btn-container">
                 <button className="payment-btn">
                   付款
                 </button>
@@ -161,7 +184,7 @@ const CheckOut = () => {
       </div>
     );
   }
-  
+
 };
 
 export default CheckOut;
