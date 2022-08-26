047aeb2e7e4f84b40671aa4d2a7753ad0ab90f65
domodfg <domodfg@yahoo.com.hk>
2022-08-26 12:11:15 +0800

    Added content to character page

--- src/Character.js
+++ src/Character.js
@@ -1,13 +1,42 @@
 import Navbar from "./components/Navbar.js";
+import "./styles/character.css";
+import cityofend from "./images/cityofend.jpeg";
+import seraphine from "./images/seraphine.jpeg";
+import roam from "./images/roam.jpeg";
+import time from "./images/time.jpeg";
+import moonisle from "./images/moonisle.jpg";
 
 const Character = () => {
+  const expand = (e) => {
+    e.currentTarget.classList.toggle("active");
+  };
   return (
     <div>
       <div className="main">
         <Navbar />
-        <h1>Hello from Character</h1>
       </div>
-  
+      <div className="character">
+        <div onClick={expand} className="country">
+          <img src={cityofend} alt="city of end" />
+          <p>終之國</p>
+        </div>
+        <div onClick={expand} className="country seraphinecontainer">
+          <img className="seraphine" src={seraphine} alt="seraphine" />
+          <p>賽拉斐爾</p>
+        </div>
+        <div onClick={expand} className="country roamcontainer">
+          <img className="roam" src={roam} alt="roam" />
+          <p>隆密</p>
+        </div>
+        <div onClick={expand} className="country timecontainer">
+          <img className="time" src={time} alt="time" />
+          <p>時之國</p>
+        </div>
+        <div onClick={expand} className="country moonislecontainer">
+          <img className="moonisle" src={moonisle} alt="moon isle" />
+          <p>新月聯邦</p>
+        </div>
+      </div>
     </div>
   );
 };
