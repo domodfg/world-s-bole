4f577c52abd183b0fe3e176433c91ac2374d8bec
plokm1234 <58145843+plokm1234@users.noreply.github.com>
2022-09-13 10:11:53 +0800

    prevent users clicking on the subs-btn multiple times

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -12,6 +12,7 @@ const PUBLIC_KEY = "jFhET9ib_PNnK18T-"
 const TEMPLATE_ID = "template_rkx54av"
 const SERVICE_ID = "service_e1c2ayo"
 
+
 export const Subscription = () => {
   const [displayAlert, setDisplayAlert] = useState(false);
   const form = useRef();
@@ -19,18 +20,30 @@ export const Subscription = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-      .then((result) => {
-        setDisplayAlert(true)
-        document.getElementsByClassName("email-addr")[0].value = ''
-      }, (error) => {
-          console.log(error.text);
-      });
+    const input = document.getElementsByClassName("email-addr")[0]
+    const subs_btn = document.getElementsByClassName("subs-btn")[0]
+    subs_btn.disabled = true
+
+    if(input.value === ''){
+      input.classList.add("error")
+      input.value = "請輸入電郵"
+      subs_btn.disabled = false
+    }else{
+      emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+        .then((result) => {
+          setDisplayAlert(true)
+          input.value = ''
+          
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
   };
 
   useEffect(() => {
     setTimeout(() => {
       setDisplayAlert(false)
+      document.getElementsByClassName("subs-btn")[0].disabled = false
     }, 7000)
   }, [displayAlert]);
 
